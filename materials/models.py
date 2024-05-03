b9b89760d67c718ef91563da9df9c96180ae7b8e
user <ibrahim42babayev@gmail.com>
2024-05-03 13:36:43 +0300

    #25.1 1.1 Try

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 class Course(models.Model):
     name = models.CharField(max_length=100,verbose_name='название')
     description = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to='materials/',verbose_name='превью',null=True,blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True,verbose_name='владелец')
 
     def __str__(self):
         return f'{self.name}'
@@ -19,6 +21,8 @@ class Subject(models.Model):
     preview = models.ImageField(upload_to='materials/',verbose_name='превью',null=True,blank=True)
     link = models.URLField(max_length=200,verbose_name='ссылка на видео',null=True,blank=True)
     course = models.ForeignKey(Course,on_delete=models.CASCADE,verbose_name='курс')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True,verbose_name='владелец')
+
 
     def __str__(self):
         return f'Урок {self.name} из курса {self.course}'
